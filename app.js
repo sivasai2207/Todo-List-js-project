d2ad70924de7c72f06b72b78c891f5af55258c76
Siva saikumar reddy k <49616092+sivasai2207@users.noreply.github.com>
2020-06-22 22:22:37 +0530

    delete and clear task function written...!

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners(e) {
   //Add task event
   form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear Task list
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 // Add task function
@@ -44,4 +50,36 @@ function addTask(e) {
   console.log(li);
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure you want to remove")) {
+      e.target.parentElement.parentElement.remove();
+    } else {
+      alert("You task is not deleted");
+    }
+  }
+}
+// clear Task
+function clearTasks(e) {
+  //  taskList.innerHTML ='';
+
+  // Faster method
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+// Filter the tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
